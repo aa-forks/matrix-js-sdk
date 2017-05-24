36e4336070c2785456b99a67fe704dbfd0425e74
Krombel <krombel@krombel.de>
2017-05-24 13:22:49 +0200

    remove duplicate code in WebsocketApi and use the code of syncApi instead

--- src/client.js
+++ src/client.js
@@ -3693,11 +3693,11 @@ MatrixClient.prototype.startClient = async function(opts) {
     };
     this._clientOpts = opts;
 
+    this._syncApi = new SyncApi(this, opts);
     if (this.useWebSockets) {
         this._websocketApi = new WebSocketApi(this, opts);
         this._websocketApi.start();
     } else {
-        this._syncApi = new SyncApi(this, opts);
         this._syncApi.sync();
     }
 };
@@ -3753,7 +3753,9 @@ MatrixClient.prototype.stopClient = function() {
 MatrixClient.prototype.connectionFallback = function(opts) {
     this.useWebSockets = false;
     console.log("Do Fallback to SyncAPI");
-    this._syncApi = new SyncApi(this, opts);
+    if (! this._syncApi) {
+        this._syncApi = new SyncApi(this, opts);
+    }
     this._syncApi.sync();
 };
 
