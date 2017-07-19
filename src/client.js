f11a7631a063daaa9b3975dc0a632c296aaea063
Krombel <krombel@krombel.de>
2017-07-19 02:32:17 +0200

    sendPresence; fix one message duplication issue
    - implement sendPresence via WebSocketApi
    - deduplicate reEmit
    - only add unique messages to "_pendingSend" (to send when WS is available)
    - replace q by bluebird

--- src/client.js
+++ src/client.js
@@ -2434,7 +2434,6 @@ MatrixClient.prototype._unstable_setStatusMessage = function(newMessage) {
  * @return {module:client.Promise} Resolves: TODO
  * @return {module:http-api.MatrixError} Rejects: with an error response.
  * @throws If 'presence' isn't a valid presence enum value.
- * TODO: Propose/Implement usage of WebSocketApi
  */
 MatrixClient.prototype.setPresence = function(opts, callback) {
     const path = utils.encodeUri("/presence/$userId/status", {
@@ -2448,6 +2447,9 @@ MatrixClient.prototype.setPresence = function(opts, callback) {
     const validStates = ["offline", "online", "unavailable"];
     if (validStates.indexOf(opts.presence) == -1) {
         throw new Error("Bad presence value: " + opts.presence);
+    }
+    if (this.useWebSockets && this._websocketApi) {
+        return this._websocketApi.sendPresence(opts);
     }
     return this._http.authedRequest(
         callback, "PUT", path, undefined, opts,
