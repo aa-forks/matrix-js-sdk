9f634edf7897aed2ebbdc47f515b2e250bbb387d
Krombel <krombel@krombel.de>
2017-08-15 00:16:26 +0200

    change: default to not using WebSockets

--- src/client.js
+++ src/client.js
@@ -210,6 +210,7 @@ function MatrixClient(opts) {
     this._syncingRetry = null;
     this._syncApi = null;
     this._websocketApi = null;
+    this.useWebSockets = Boolean(opts.useWebSockets);
     this._peekSync = null;
     this._isGuest = false;
     this._ongoingScrollbacks = {};
@@ -217,10 +218,6 @@ function MatrixClient(opts) {
     this.urlPreviewCache = {};
     this._notifTimelineSet = null;
 
-    this.useWebSockets = true;
-    if (opts.useWebSockets) {
-        this.useWebSockets = Boolean(opts.useWebSockets);
-    }
 
     this._crypto = null;
     this._cryptoStore = opts.cryptoStore;
